d2d98b71c5fd405c421c47952adc8c5a4890b670
Seyon <seyon.vasantharajan@gmail.com>
2014-08-04 17:22:07 -0400

    Comepleted Version 1 of gem
    -Added case statement that takes an uppercase letter
    and returns an Animal Name that corresponds to that letter
    -To Add to next version
      ->Letter can be upper or lowercase
      ->If another string is passed(longer than one character),   output
    Default animal: human

--- lib/animal_names.rb
+++ lib/animal_names.rb
@@ -1,5 +1,66 @@
 require "animal_names/version"
 
 module AnimalNames
-  # Your code goes here...
+  #In next version add constraint that restricts
+  #the string letter to being one letter
+  def self.animal(letter)
+
+    case letter
+      when 'A'
+        puts 'Aardvark'
+      when 'B'
+        puts 'Bee'
+      when 'C'
+        puts 'Cat'
+      when 'D'
+        puts 'Dog'
+      when 'E'
+        puts 'Elephant'
+      when 'F'
+        puts 'Frog'
+      when 'G'
+        puts 'Gorilla'
+      when 'H'
+        puts 'Hamster'
+      when 'I'
+        puts 'Iguana'
+      when 'J'
+        puts 'Jaguar'
+      when 'K'
+        puts 'Koala'
+      when 'L'
+        puts 'Lion'
+      when 'M'
+        puts 'Mammoth'
+      when 'N'
+        puts 'Narwhal'
+      when 'O'
+        puts 'Orangutan'
+      when 'P'
+        puts 'Panda'
+      when 'Q'
+        puts 'Quail'
+      when 'R'
+        puts 'Rhinoceros'
+      when 'S'
+        puts 'Snake'
+      when 'T'
+        puts 'Tarantula'
+      when 'U'
+        puts 'Uakari'
+      when 'V'
+        puts 'Vulture'
+      when 'W'
+        puts 'Whale'
+      when 'X'
+        puts 'X-ray Tetra'
+      when 'Y'
+        puts 'Yak'
+      when 'Z'
+        puts 'Zebra'
+      else
+        puts 'Invalid Input!'
+
+    end
+  end
 end
